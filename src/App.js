8a51c3f3975422c60df29132438d39eeae1610ad
Tlathrop01 <tlathrop@unomaha.edu>
2025-11-16 20:41:39 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
